9674495bb8dee81aa59df625e037820f52d86a9a
Angie Thai <angiethai@Angies-MBP.attlocal.net>
2022-05-12 19:44:57 -0700

    adjusted counter outside loop

--- root/scripts/editRecipe.js
+++ root/scripts/editRecipe.js
@@ -100,8 +100,9 @@ async function fillRecipePage(recipeId) {
   for (let k = 1; k <= recipe.steps.length; k += 1) {
     if (k > 5) {
       addStep();
+    } else {
       i += 1;
-    } 
+    }
     console.log(recipe.steps.length);
     const stepVal = document.getElementsByClassName('step')[k - 1];
     stepVal.value = recipe.steps[k - 1].step;

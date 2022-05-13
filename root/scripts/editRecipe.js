eea9aa47fb4dee9dafdb6d12d7034074bf672d1d
Angie Thai <angiethai@Angies-MBP.attlocal.net>
2022-05-12 19:43:20 -0700

    moved counter to inside loop

--- root/scripts/editRecipe.js
+++ root/scripts/editRecipe.js
@@ -100,12 +100,12 @@ async function fillRecipePage(recipeId) {
   for (let k = 1; k <= recipe.steps.length; k += 1) {
     if (k > 5) {
       addStep();
+      i += 1;
     } 
     console.log(recipe.steps.length);
     const stepVal = document.getElementsByClassName('step')[k - 1];
     stepVal.value = recipe.steps[k - 1].step;
     console.log(stepVal.value);
-    i = recipe.steps.length + 1;
   }
 
   document.getElementById('cheap').checked = recipe.cheap;

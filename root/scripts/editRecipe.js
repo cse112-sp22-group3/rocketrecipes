7ff91f35cc9d934399b95dedbc7fce36323126d1
Angie Thai <angiethai@Angies-MBP.attlocal.net>
2022-05-12 18:47:05 -0700

    changed step value tracking

--- root/scripts/editRecipe.js
+++ root/scripts/editRecipe.js
@@ -98,10 +98,12 @@ async function fillRecipePage(recipeId) {
   }
 
   for (let k = 1; k <= recipe.steps.length; k += 1) {
-    if (k > 5) {
+    /*if (k > 5) {
       addStep();
-    }
-    const stepVal = document.getElementsByClassName('step')[k - 1];
+    }*/
+    addStep();
+    //const stepVal = document.getElementsByClassName('step')[k - 1];
+    const stepVal = document.getElementById(`Step${k.toString()}`);
     stepVal.value = recipe.steps[k - 1].step;
   }
 

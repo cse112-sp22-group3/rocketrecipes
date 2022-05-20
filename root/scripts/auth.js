a4af381af8484c72315f0265aa2b5abb8dfe3916
Luis Ramos <lr2471311@gmail.com>
2022-05-19 17:35:24 -0700

    added new lines at the end of files

--- root/scripts/auth.js
+++ root/scripts/auth.js
@@ -2,20 +2,17 @@ const loginBtn = document.getElementById('login');
 const signupBtn = document.getElementById('signup');
 
 loginBtn.addEventListener('click', (e) => {
-	let parent = e.target.parentNode.parentNode;
-	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
-			signupBtn.parentNode.classList.add('slide-up')
-			parent.classList.remove('slide-up')
-		//}
-	});
+    let parent = e.target.parentNode.parentNode;
+    Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+        signupBtn.parentNode.classList.add('slide-up')
+        parent.classList.remove('slide-up')
+    });
 });
 
-
-    signupBtn.addEventListener('click', (e) => {
-        let parent = e.target.parentNode;
-        Array.from(e.target.parentNode.classList).find((element) => {
-                loginBtn.parentNode.parentNode.classList.add('slide-up')
-                parent.classList.remove('slide-up')
-
-        });
+signupBtn.addEventListener('click', (e) => {
+    let parent = e.target.parentNode;
+    Array.from(e.target.parentNode.classList).find((element) => {
+        loginBtn.parentNode.parentNode.classList.add('slide-up')
+        parent.classList.remove('slide-up')
     });
+});

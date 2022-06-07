1152a2c963c68346606aef918f12204a865db4d8
Angie Thai <angiethai@Angies-MBP.attlocal.net>
2022-06-07 01:08:31 -0700

    replaced waitFor with waitForTimeout

--- testing/e2e/favoriteRecipe.test.js
+++ testing/e2e/favoriteRecipe.test.js
@@ -1,4 +1,4 @@
-let rootUrl = 'rocketrecipesv2.netlify.app';
+let rootUrl = '127.0.0.1:5500';
 const pullRequestId = process.env.GITHUB_PR_NUMBER;
 
 beforeAll(async() => {
@@ -26,13 +26,15 @@ describe('favorite a recipe', () => {
         const title = await page.evaluate(() => document.querySelector('#recipe-title').textContent);
         await expect(page.title()).resolves.toMatch(title);
 
-        await page.waitFor(500);
-        await page.click('#fav-icon');
+        await page.waitForTimeout(2000);
+        let favIcon = await page.evaluateHandle( () => document.querySelector("body > main > div > div.recipe-header > div > div.recipe-button-container > button#fav-icon.recipe-button"));
+        await favIcon.click();
+        await page.waitForTimeout(1000);
     });
 
 
     it('should display favorite recipe in my account', async() => {
-        await page.goto(`http://${rootUrl}/root/html/generalaccount`);
+        await page.goto(`http://${rootUrl}/root/html/generalAccount.html`);
 
         await page.waitForSelector('body > main > div.FavoriteFood > recipe-card');
         const favs = await page.$$('body > main > div.FavoriteFood > recipe-card');
@@ -47,12 +49,12 @@ describe('favorite a recipe', () => {
         const title = await page.evaluate(() => document.querySelector('#recipe-title').textContent);
         await expect(page.title()).resolves.toMatch(title);
 
-        await page.waitFor(500);
+        await page.waitForTimeout(500);
         await page.click('#fav-icon');
     });
 
     it('should remove recipe from my account', async() => {
-        await page.goto(`http://${rootUrl}/root/html/generalaccount`);
+        await page.goto(`http://${rootUrl}/root/html/generalAccount.html`);
 
         await page.waitForSelector('body > main > div.FavoriteFood');
         const favs = await page.$$('body > main > div.FavoriteFood > recipe-card');

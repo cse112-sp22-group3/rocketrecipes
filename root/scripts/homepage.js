0c4bca1fc53cfa409726b79efad4f7197f2b1148
gmihir <m08.in.us@gmail.com>
2022-06-07 02:12:49 -0700

    fixed issue angie pointed out

--- root/scripts/homepage.js
+++ root/scripts/homepage.js
@@ -15,8 +15,7 @@ function createRecommendedRecipes() {
   // integrate user preferences
   const userPreferences = JSON.parse(localStorage.getItem('user-preferences'));
   let numAdded = 0;
-
-  if (userPreferences != null) {
+  if (userPreferences != null && userPreferences.length > 0) {
     const index = Math.floor(Math.random() * allRecipes.length - 1);
     for (let i = index; i < allRecipes.length && numAdded < 8; i += 1) {
       const recipe = allRecipes[i];
@@ -38,8 +37,9 @@ function createRecommendedRecipes() {
         numAdded += 1;
       }
     }
-  } else if (numAdded < numRecipes) {
-    for (let i = 0; numAdded < 8; i += 1) {
+  }
+  if (numAdded < numRecipes) {
+    for (let i = 0; numAdded < numRecipes; i += 1) {
       const recipeCard = document.createElement('recipe-card');
       recipeCard.data = allRecipes[randomNumber + i];
 

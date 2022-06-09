fb7cade4195654a8b20b33c37295b280918d4cbf
helloserina <skhanna@ucsd.edu>
2022-06-08 18:18:27 -0700

    amount accepts decimal

--- root/scripts/editRecipe.js
+++ root/scripts/editRecipe.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-eval */
 /* eslint-disable import/extensions */
 /* eslint-disable prefer-destructuring */
 import {
@@ -180,7 +181,12 @@ async function init() {
     for (let j = 0; j < document.getElementsByClassName('Ingre').length; j += 1) {
       const ingredientInfo = {};
       ingredientInfo.name = whitespaceTrimmer(purifyDOM(document.getElementsByClassName('Ingredient')[j].value));
-      ingredientInfo.amount = parseInt(document.getElementsByClassName('Ingre')[j].value, 10);
+      ingredientInfo.amount = whitespaceTrimmer(purifyDOM(document.getElementsByClassName('Ingre')[j].value));
+      try {
+        ingredientInfo.amount = eval(ingredientInfo.amount);
+      } catch (err) {
+        ingredientInfo.amount = parseFloat(ingredientInfo.amount);
+      }
       ingredientInfo.unit = whitespaceTrimmer(purifyDOM(document.getElementsByClassName('unit')[j].value));
       userGenRecipe.ingredients.push(ingredientInfo);
       numIngredients += 1;
